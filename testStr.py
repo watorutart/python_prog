945c65987fa2e5715db4567d186eab4e08d0d61f
watorutart <46208434+watorutart@users.noreply.github.com>
2019-03-20 17:51:01 +0900

    Update testStr.py

--- testStr.py
+++ testStr.py
@@ -13,3 +13,22 @@ print(s[::-1])  # s[9:-1:-1]は×
 year = 2020
 s = f"東京オリンピックは{year}年です"
 print(s)
+
+yen = 228080
+print(f"{yen:,}円")
+
+n = 255
+print(f"n -> 10進数：{n:d}")
+print(f"n -> 2進数：{n:b}")
+print(f"n -> 8進数：{n:o}")
+print(f"n -> 16進数：{n:x}")
+
+print(f"1/3 -> {1/3:.3f}")
+print(f"1/3 -> {1/3:.5f}")
+
+print(f"{0.377:.2%}")
+
+s = "Hello Python, Hello world"
+print(s.find("Python"))
+print(s.find("Hello"))
+print(s.find("Hello",3))
