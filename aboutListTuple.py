6042d7e16af1fad024ccba838d209ad5eb6496da
watorutart <46208434+watorutart@users.noreply.github.com>
2019-03-28 10:26:54 +0900

    Update aboutListTuple.py

--- aboutListTuple.py
+++ aboutListTuple.py
@@ -38,3 +38,37 @@ t1 = ("abc",) # これはタプル
 t2 = ("abc")  # これは文字列
 print("t1 type = " + str(type(t1)))
 print("t2 type = " + str(type(t2)))
+
+# リストの内包表記
+months = [str(m) + "月" for m in range(1, 13)]
+print(months)
+
+heisei = [13, 15, 20, 3, 7]
+seireki = [y + 1988 for y in heisei]
+print(seireki)
+
+colors1 = ["white", "black", "red", "blue"]
+colors2 = [f"{i}:{v}" for i, v in enumerate(colors1)]
+print(colors2)
+
+nums1 = [6, 2, 53, 72, 83, 16, 20, 41, 93]
+nums2 = [n for n in nums1 if n % 2 == 0]
+print(nums2)
+
+names = [("abe", "male"), ("baba", "male"), ("caty", "female"), ("daigo", "male"), ("eibon", "female")]
+man1 = list(filter(lambda n: n[1] == "male", names))
+print("man1 = " + str(man1))
+man2 = [n for n in names if n[1] == "male"]
+print("man2 = " + str(man2))
+
+rate = 108
+dolls = [9.5, 2.0, 13.8, 17.0, 6.6, 19.0]
+yens1 = list(map(lambda d: d * rate, dolls))
+print("yens1 = " + str(yens1))
+yens2 = [d * rate for d in dolls]
+print("yens2 = " + str(yens2))
+
+yens3 = list(map(lambda d: d * rate, filter(lambda d: d >= 10, dolls)))
+print(yens3)
+yens4 = [d * rate for d in dolls if d >= 10]
+print(yens4)
