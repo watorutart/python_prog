72cc0ef3d4fbd719838c5b5a31d09e160f215da0
watorutart <46208434+watorutart@users.noreply.github.com>
2019-04-02 22:36:49 +0900

    Create window.py

---
+++ pygame/window.py
@@ -0,0 +1,21 @@
+import sys
+import pygame
+from pygame.locals import *
+
+pygame.init() # pygameの初期化
+screen = pygame.display.set_mode((400, 300)) # ウインドウの大きさ
+pygame.display.set_caption("PyGame") # タイトルバー
+
+# mainループ
+def main():
+    while True:
+        screen.fill((0, 0, 0)) # ウインドウの背景色
+        # イベントの取得
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit() # 閉じるボタンが押されたらプログラムを終了
+                sys.exit
+        pygame.display.update()
+
+if __name__ == "__main__":
+    main()
